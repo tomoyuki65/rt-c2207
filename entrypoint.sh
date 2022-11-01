cc3cfa6d4e42d109553eb219f6c0b27f2d4e3fe6
tomoyuki65 <tomoyuki65@users.noreply.github.com>
2022-11-01 18:24:32 +0900

    [skip ci] modify entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ rm -f /rt-c2207/tmp/pids/server.pid
 if [ "$RAILS_ENV" = "production" ]; then
   bundle exec rails assets:clobber
   bundle exec rails assets:precompile
+  bundle exec rails db:migrate
 fi
 
 # サーバー実行(DockerfileのCMDをセット)
